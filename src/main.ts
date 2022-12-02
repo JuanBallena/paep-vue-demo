ee5a162cc334435590f94b6b93bef87a164f5724
JuanBallena <jpbu911@hotmail.com>
2022-12-02 17:57:58 -0500

    remove config json

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,29 @@ import { VueClipboard } from '@soerenmartius/vue3-clipboard'
 
 const app = createApp(App)
 
-fetch("/config.json")
-  .then((response) => {
-    response.json().then((config: any) => {
+// fetch("/config.json")
+//   .then((response) => {
+//     response.json().then((config: any) => {
        
-        app.provide('$baseUrlApi', config.BASE_URL_API);
-        app.provide('$itemsPerPageEncuestas', config.ITEMS_PER_PAGE_ENCUESTAS);
-        app.provide('$itemsPerPageUsuarios', config.ITEMS_PER_PAGE_USUARIOS);
-        app.provide('$itemsPerPageEncuestasCulminadas', config.ITEMS_PER_PAGE_ENCUESTAS_CULMINADAS);
+//         app.provide('$baseUrlApi', config.BASE_URL_API);
+//         app.provide('$itemsPerPageEncuestas', config.ITEMS_PER_PAGE_ENCUESTAS);
+//         app.provide('$itemsPerPageUsuarios', config.ITEMS_PER_PAGE_USUARIOS);
+//         app.provide('$itemsPerPageEncuestasCulminadas', config.ITEMS_PER_PAGE_ENCUESTAS_CULMINADAS);
 
-        app.use(store);
-        app.use(router);
-        app.use(VueClipboard);
-        app.mount('#app');
+//         app.use(store);
+//         app.use(router);
+//         app.use(VueClipboard);
+//         app.mount('#app');
 
-    })
-});
+//     })
+// });
 
+app.provide('$baseUrlApi', '');
+app.provide('$itemsPerPageEncuestas', 10);
+app.provide('$itemsPerPageUsuarios', 10);
+app.provide('$itemsPerPageEncuestasCulminadas', 10);
+
+app.use(store);
+app.use(router);
+app.use(VueClipboard);
+app.mount('#app');
