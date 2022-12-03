5c7cf9dc1734338ba0eee63e1c3c89e9ff95334b
JuanBallena <jpbu911@hotmail.com>
2022-12-02 22:15:38 -0500

    New Deployment

---
+++ js/chunk-2d0b2986.5ecf8ef6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2986"],{"257d":function(c,n,e){"use strict";e.r(n);var r=e("7a23");const t={class:"g-error-page"},a=Object(r["h"])("div",null,[Object(r["h"])("div",null,[Object(r["h"])("i",{class:"fas fa-exclamation-circle"})]),Object(r["h"])("br"),Object(r["i"])(" Error 404 - Página no encontrada ")],-1),i=[a];function o(c,n){return Object(r["v"])(),Object(r["g"])("div",t,i)}var b=e("6b0d"),s=e.n(b);const d={},l=s()(d,[["render",o]]);n["default"]=l}}]);
+//# sourceMappingURL=chunk-2d0b2986.5ecf8ef6.js.map
